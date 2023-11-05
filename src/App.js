02070b306dca35feab746a86612ec613f99cc26e
Donking45 <kingsleyokon610@gmail.com>
2023-11-04 20:13:25 -0400

    Implemented NFT metadata retrieval, floor price checking, transaction status check, and address-specific NFT transfer history in App.js.

--- src/App.js
+++ src/App.js
@@ -3,38 +3,80 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
-
 function App() {
-  const [transactions, setTransactions] = useState();
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      setTransactions(await alchemy.core.getTransactions());
+      // Get NFT Metadata for a specific contract address and token ID
+      const contractAddress = '0xYourContractAddress';
+      const tokenId = 'YourTokenId';
 
-      
+      const metadata = await fetchNFTMetadata(contractAddress, tokenId);
+
+      // Check the floor price of an NFT from an NFT marketplace
+      const floorPrice = await checkNFTFloorPrice('YourCollectionSlug');
+
+      // Check if a pending transaction got mined
+      const transactionHash = '0xYourTransactionHash';
+      const isMined = await checkTransactionMined(transactionHash);
+
+      // Get NFT transfer history for an address
+      const address = '0xYourAddress';
+      const transferHistory = await getNFTTransferHistory(address);
+
+      // Update the state with the results
+      setTransactions({
+        metadata,
+        floorPrice,
+        isMined,
+        transferHistory,
+      });
     }
 
     fetchData();
-  }, [transactions]);
+  }, []);
 
-  return <div className="App">Transactions: {transactions}</div>;
+  return (
+    <div className="App">
+      <h1>NFT Information</h1>
+      <p>Metadata: {transactions.metadata}</p>
+      <p>Floor Price: {transactions.floorPrice}</p>
+      <p>Transaction Mined: {transactions.isMined ? 'Yes' : 'No'}</p>
+      <p>Transfer History: {transactions.transferHistory}</p>
+    </div>
+  );
 }
 
+async function fetchNFTMetadata(contractAddress, tokenId) {
+  // Implement the logic to fetch NFT metadata here
+  // You can use Alchemy to call the tokenURI function and then fetch the metadata
+  // Return the metadata
+}
+
+async function checkNFTFloorPrice(collectionSlug) {
+  // Implement the logic to check the floor price of an NFT from a marketplace
+  // You can use the marketplace API to fetch the floor price
+  // Return the floor price
+}
+
+async function checkTransactionMined(transactionHash) {
+  // Implement the logic to check if a pending transaction got mined
+  // You can use Alchemy to get the transaction receipt and check the block number
+  // Return true if mined, false if pending
+}
+
+async function getNFTTransferHistory(address) {
+  // Implement the logic to get NFT transfer history for an address
+  // You can use event listening to fetch the transfer history
+  // Return the transfer history
+}
 
 export default App;
